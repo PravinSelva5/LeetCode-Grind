eebf8ff056824fa84d1a2940f895def3577d301c
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-05-16 22:11:11 -0400

    updated code

--- MoveZeros.py
+++ MoveZeros.py
@@ -8,15 +8,15 @@ class Solution:
         ptr = 0
         search_ptr = 0
         len_arr = len(nums)
-        
-        for i in range(len(nums)):
+
+        for i in range(len_arr):
             if nums[search_ptr] != 0:
                 nums[ptr] = nums[search_ptr]
                 search_ptr += 1
                 ptr += 1
             else:
                 search_ptr += 1
-        
-        for j in range(ptr,len_arr):
+
+        for j in range(ptr, len_arr):
             nums[ptr] = 0
             ptr += 1
