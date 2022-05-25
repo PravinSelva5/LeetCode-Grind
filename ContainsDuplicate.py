1c183b9d87df1c8a62eb67d19c62d4470bfceeb8
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-05-25 19:50:55 -0400

    completed Two Sum LC question

---
+++ ContainsDuplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        m = {}
+
+        for num in nums:
+            if num in m:
+                m[num] += 1
+            else:
+                m[num] = 1
+
+        for i in m:
+            if m[i] > 1:
+                return True
+
+        return False
