cc79da4af344335d8b25e0d6b62a2a6c4a8904e5
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-05-30 21:39:59 -0400

    completed Linked List Cycle LC question

---
+++ LinkedListCycle.py
@@ -0,0 +1,21 @@
+# This problem can be solved with the fast & slow pointers pattern
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_ptr = head
+        fast_ptr = head
+
+        while slow_ptr and fast_ptr and fast_ptr.next:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+            if fast_ptr == slow_ptr:
+                return True
+
+        return False
