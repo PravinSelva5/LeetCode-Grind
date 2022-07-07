67082e23dda01b220eed064c5c18bd87ea313ab7
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-07-07 19:50:59 -0400

    re-did valid anagram leetcode question, different solution included in comments

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -21,3 +21,31 @@ class Solution:
                 return False
 
         return True
+
+'''
+Another Solution:
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        m = {}
+        
+        for letter in s:
+            if letter not in m:
+                m[letter] = 0
+            m[letter] += 1
+        
+        for char in t:
+            if char in m:
+                m[char] -= 1
+            else:
+                m[char] = 1
+        
+        for char, occurence in m.items():
+            if occurence != 0:
+                return False
+            
+            
+        return True
+
+
+'''
