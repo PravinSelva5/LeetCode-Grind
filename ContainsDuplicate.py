cc634ed953397e6ca39faef28022df9718d39c91
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-07-06 22:09:52 -0400

    added another solution to contains duplicates answer

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -14,3 +14,24 @@ class Solution:
                 return True
 
         return False
+
+
+# Another Solution:
+
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         m = {}
+        
+#         for num in nums:
+#             if num not in m:
+#                 m[num] = 0
+#             m[num] += 1
+        
+#         for num, value in m.items():
+#             if value > 1:
+#                 return True
+#         return False
+
+
+
