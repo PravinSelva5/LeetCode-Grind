4231d357e6a7bb932a332b3d0f54da2660dc1b00
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-08-03 22:14:52 -0400

    re-tried valid parenthesis leetcode question again

--- ValidParenthesis.py
+++ ValidParenthesis.py
@@ -20,3 +20,29 @@ class Solution:
             stack.append(character)
 
         return len(stack) == 0
+
+
+# Solution/ Attempt 2
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         brackets = {
+#             ')': '(',
+#             '}': '{',
+#             ']': '['
+#         }
+        
+#         for char in s:
+#             if char in brackets:
+#                 # if true, that means its a closing bracket
+#                 if stack and stack[-1] == brackets[char]:
+#                     stack.pop()
+                
+#                 else:
+#                     return False
+#             else:
+#                 stack.append(char)
+        
+        
+#         # Only return true if stack is empty
+#         return True if not stack else False
