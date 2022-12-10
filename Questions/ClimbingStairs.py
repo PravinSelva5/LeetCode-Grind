8cdd3881dbfefd430f09af1b2aa18add1dcd0ed4
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-12-10 17:08:32 -0500

    worked on the climbing stairs leetcode question

--- Questions/ClimbingStairs.py
+++ Questions/ClimbingStairs.py
@@ -13,3 +13,19 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]
+    
+
+# Another Solution
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         one, two = 1, 1
+
+#         for i in range(n-1):
+#             temp = one
+#             one = one + two
+#             two = temp 
+        
+#         return one
+
+
