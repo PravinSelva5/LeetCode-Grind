dc908c96d2b75011aa422c6d69cc55c9b64683e2
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2023-01-03 22:10:41 -0500

    worked on Kth Largest Element in a Stream

--- Medium Questions/KthLargestElementInAStream.py
+++ Medium Questions/KthLargestElementInAStream.py
@@ -1,13 +1,17 @@
 class KthLargest:
-    
-    def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.nums = nums
 
-    def add(self, val: int) -> int:
+    def __init__(self, k: int, nums: List[int]):
+        # minHeap with K largest integers
+        self.minHeap = nums
+        self.k = k
+        # this is how to turn minHeap to a heap
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
         
 
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
